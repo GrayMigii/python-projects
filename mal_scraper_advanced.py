5b43e3991ab90f931c41ec861aed8894cae1fbc8
graymigii <abdullahkassa@gmail.com>
2023-04-24 16:24:36 +0200

    done

--- mal_scraper_advanced.py
+++ mal_scraper_advanced.py
@@ -8,19 +8,21 @@ import math
 
 arg_list = sys.argv
 df_length = 50 if len(arg_list) == 1 else int(arg_list[1])
+pd.options.display.max_rows = df_length
 
 titles = []
 unf_scores = []
 dict = {'Title': [], 'Score': []}
 
-for i in range(math.ceil(df_length/50)):
+# loops through the mal top anime pages
+for i in range(1, math.ceil(df_length/50) + 1):
     if i == 1:
         source = requests.get('https://myanimelist.net/topanime.php').text
     else:
         source = requests.get(f'https://myanimelist.net/topanime.php?limit={50*i}').text
 
-    soup = BeautifulSoup(source, 'lxml')
     # lxml is a parser that needs to be installed first
+    soup = BeautifulSoup(source, 'lxml')
 
     # finds all titles and scores from the top anime mal page
     titles += soup.find_all('h3', class_='hoverinfo_trigger')
