7d09963d17f1f0619201479acdd44884cad50e7a
graymigii <abdullahkassa@gmail.com>
2023-04-24 16:35:43 +0200

    done

--- mal_scraper.py
+++ mal_scraper.py
@@ -3,26 +3,43 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import sys
+import math
 
+arg_list = sys.argv
+df_length = 50 if len(arg_list) == 1 else int(arg_list[1])
+pd.options.display.max_rows = df_length
+
+titles = []
+unf_scores = []
 dict = {'Title': [], 'Score': []}
 
-source = requests.get('https://myanimelist.net/topanime.php').text
+# loops through the mal top anime pages
+for i in range(1, math.ceil(df_length/50) + 1):
+    # checks which pages to scrape from
+    if i == 1:
+        source = requests.get('https://myanimelist.net/topanime.php').text
+    else:
+        source = requests.get(f'https://myanimelist.net/topanime.php?limit={50*i}').text
 
-soup = BeautifulSoup(source, 'lxml')
-# lxml is a parser that needs to be installed first
+    # lxml is a parser that needs to be installed first
+    soup = BeautifulSoup(source, 'lxml')
 
-# finds all titles and scores from the top anime mal page
-titles = soup.find_all('h3', class_='hoverinfo_trigger')
-scores = soup.find_all('span', class_='score-label')
+    # finds all titles and scores from the top anime mal page
+    titles += soup.find_all('h3', class_='hoverinfo_trigger')
+    unf_scores += soup.find_all('span', class_='score-label')
+
+# filters out the 'N/A' scores
+scores = list(filter(lambda x: 'N/A' not in x, unf_scores))
 
 # loops through the titles and scores and appends them to the dictionary
-for title in titles:
+for title in titles[:df_length]:
     dict['Title'].append(title.text)
 
-for score in scores:
-    dict['Score'].append(score.text) if score.text != 'N/A' else None
+for score in scores[:df_length]:
+    dict['Score'].append(score.text)
 
 # turns the dictionary into a dataframe
-df = pd.DataFrame(dict, index=[x for x in range(1, 51)])
+df = pd.DataFrame(dict, index=[x for x in range(1, df_length + 1)])
 
 print(df)
